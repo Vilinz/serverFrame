67d543d537c1b9109fefd77dff58351fa9d05edf
Vilinz <1622070923@qq.com>
2020-06-03 02:17:23 -0700

    配置系统和配置变更

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -2,11 +2,24 @@
 #include "vilin/log/log.h"
 #include <yaml-cpp/yaml.h>
 
+#if 0
 vilin::ConfigVar<int>::ptr g_int_value_config = vilin::Config::Lookup("system.port", (int)8080, "system port");
+
+vilin::ConfigVar<float>::ptr g_int_valuex_config = vilin::Config::Lookup("system.port", (float)8080, "system port");
 
 vilin::ConfigVar<float>::ptr g_float_value_config = vilin::Config::Lookup("system.value", (float)1.1f, "system value");
 
 vilin::ConfigVar<std::vector<int>>::ptr g_int_vec_value_config = vilin::Config::Lookup("system.int_vec", std::vector<int>{1,2}, "system int vector");
+
+vilin::ConfigVar<std::list<int>>::ptr g_int_list_value_config = vilin::Config::Lookup("system.int_list", std::list<int>{1,2}, "system int list");
+
+vilin::ConfigVar<std::set<int>>::ptr g_int_set_value_config = vilin::Config::Lookup("system.int_set", std::set<int>{1,2}, "system int set");
+
+vilin::ConfigVar<std::unordered_set<int>>::ptr g_int_uset_value_config = vilin::Config::Lookup("system.int_uset", std::unordered_set<int>{1,2}, "system int uset");
+
+vilin::ConfigVar<std::map<std::string, int>>::ptr g_str_int_map_value_config = vilin::Config::Lookup("system.str_int_map", std::map<std::string, int>{{"k",2}}, "system str int map");
+
+vilin::ConfigVar<std::unordered_map<std::string, int>>::ptr g_str_int_umap_value_config = vilin::Config::Lookup("system.str_int_umap", std::unordered_map<std::string, int>{{"k",2}}, "system str int umap");
 
 void print_yaml(const YAML::Node& node, int level) {
 	if(node.IsScalar()) {
@@ -41,10 +54,30 @@ void test_config() {
 	VILIN_LOG_INFO(VILIN_LOG_ROOT()) << "before: " << g_int_value_config->getValue();
 	VILIN_LOG_INFO(VILIN_LOG_ROOT()) << "before: " << g_float_value_config->getValue();
 
-	auto v = g_int_vec_value_config->getValue();
-	for(auto &i : v) {
-		VILIN_LOG_INFO(VILIN_LOG_ROOT()) << "before int_vec " << i;
+#define XX(g_var, name, prefix) \
+	{ \
+		auto v = g_var->getValue(); \
+		for(auto& i : v) { \
+			VILIN_LOG_INFO(VILIN_LOG_ROOT()) << #prefix " " #name ": " << i; \
+		} \
+		VILIN_LOG_INFO(VILIN_LOG_ROOT()) << #prefix " " #name " yaml: \n" << g_var->toString(); \
 	}
+
+#define XX_M(g_var, name, prefix) \
+	{ \
+		auto v = g_var->getValue(); \
+		for(auto& i : v) { \
+			VILIN_LOG_INFO(VILIN_LOG_ROOT()) << #prefix " " #name ": {" << i.first << " - " << i.second << "}"; \
+		} \
+		VILIN_LOG_INFO(VILIN_LOG_ROOT()) << #prefix " " #name " yaml: \n" << g_var->toString(); \
+	}
+
+	XX(g_int_vec_value_config, int_vec, before);
+	XX(g_int_list_value_config, int_list, before);
+	XX(g_int_set_value_config, int_set, before);
+	XX(g_int_uset_value_config, int_uset, before);
+	XX_M(g_str_int_map_value_config, str_int_map, before);
+	XX_M(g_str_int_umap_value_config, str_int_umap, before);
 
 	YAML::Node root = YAML::LoadFile("/home/vilingif/Desktop/serverFrame/bin/conf/log.yml");
 	vilin::Config::LoadFromYaml(root);
@@ -52,16 +85,109 @@ void test_config() {
 	VILIN_LOG_INFO(VILIN_LOG_ROOT()) << "after: " << g_int_value_config->getValue();
 	VILIN_LOG_INFO(VILIN_LOG_ROOT()) << "after: " << g_float_value_config->getValue();
 
-	v = g_int_vec_value_config->getValue();
-	for(auto &i : v) {
-		VILIN_LOG_INFO(VILIN_LOG_ROOT()) << "after int_vec " << i;
+	XX(g_int_vec_value_config, int_vec, after);
+	XX(g_int_list_value_config, int_list, after);
+	XX(g_int_set_value_config, int_set, after);
+	XX(g_int_uset_value_config, int_uset, after);
+	XX_M(g_str_int_map_value_config, str_int_map, after);
+	XX_M(g_str_int_umap_value_config, str_int_umap, after);
+}
+#endif
+
+class Person {
+public:
+	std::string m_name;
+	int m_age = 0;
+	bool m_sex = 0;
+	std::string toString() const {
+		std::stringstream ss;
+		ss << "{Person name=" << m_name
+			 << " age=" << m_age
+			 << " sex=" << m_sex
+			 << "}";
+		return ss.str();
 	}
+
+	bool operator==(const Person& oth) const {
+		return m_name == oth.m_name
+			&& m_age == oth.m_age
+			&& m_sex == oth.m_sex;
+	}
+};
+
+namespace vilin {
+
+//--------------person
+template<>
+class LexicalCast<std::string, Person> {
+public:
+	Person operator() (const std::string& v) {
+		YAML::Node node = YAML::Load(v);
+		Person p;
+		p.m_name = node["name"].as<std::string>();
+		p.m_age = node["age"].as<int>();
+		p.m_sex = node["sex"].as<bool>();
+		return p;
+	}
+};
+
+template<>
+class LexicalCast<Person, std::string> {
+public:
+	std::string operator() (const Person& p) {
+		YAML::Node node;
+		node["name"] = p.m_name;
+		node["age"] = p.m_age;
+		node["sex"] = p.m_sex;
+		std::stringstream ss;
+		ss << node;
+		return ss.str();
+	}
+};
+
+}
+
+vilin::ConfigVar<Person>::ptr g_person = vilin::Config::Lookup("class.person", Person(), "system person");
+
+vilin::ConfigVar<std::map<std::string, Person>>::ptr g_person_map = vilin::Config::Lookup("class.map", std::map<std::string, Person>{}, "system map");
+
+vilin::ConfigVar<std::map<std::string, std::vector<Person>>>::ptr g_person_vec_map = vilin::Config::Lookup("class.vec_map", std::map<std::string, std::vector<Person>>{}, "system vec map");
+
+
+void test_class() {
+	VILIN_LOG_INFO(VILIN_LOG_ROOT()) << "before " << g_person->getValue().toString() << " - " << g_person->toString();
+
+#define XX_PM(g_var, prefix) \
+	{ \
+		auto m = g_person_map->getValue(); \
+		for(auto& i : m) { \
+			VILIN_LOG_INFO(VILIN_LOG_ROOT()) << prefix << ": " << i.first << " - " << i.second.toString(); \
+		} \
+		VILIN_LOG_INFO(VILIN_LOG_ROOT()) << prefix << ": size=" << m.size(); \
+	}
+
+	g_person->addListener(10, [](const Person& old_value, const Person& new_value) {
+		VILIN_LOG_INFO(VILIN_LOG_ROOT()) << "old_value=" << old_value.toString()
+			<< " new_value=" << new_value.toString();
+	});
+
+	XX_PM(g_person_map, "class.map.before");
+	VILIN_LOG_INFO(VILIN_LOG_ROOT()) << "before: " << g_person_vec_map->toString();
+
+	YAML::Node root = YAML::LoadFile("/home/vilingif/Desktop/serverFrame/bin/conf/log.yml");
+	vilin::Config::LoadFromYaml(root);
+
+	VILIN_LOG_INFO(VILIN_LOG_ROOT()) << "after " << g_person->getValue().toString() << " - " << g_person->toString();
+
+	XX_PM(g_person_map, "class.map.after");
+	VILIN_LOG_INFO(VILIN_LOG_ROOT()) << "after: " << g_person_vec_map->toString();
 }
 
 int main(int argc, char** argv) {
 	
 
 	//test_yaml();
-	test_config();
+	// test_config();
+	test_class();
 	return 0;
 }
